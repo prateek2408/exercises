9feb28c0a9688461585cbb89ad841b64c0765b63
Prateek Khushalani <prateek.khushalani@gmail.com>
2022-04-20 17:37:21 +0530

    adding first exercise

---
+++ hclwrite/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"sort"
+
+	"github.com/hashicorp/hcl/v2"
+	"github.com/hashicorp/hcl/v2/hclwrite"
+	"github.com/zclconf/go-cty/cty"
+)
+
+func main() {
+	fmt.Println("hello WOrld")
+	basePath, err := os.Getwd()
+	var newFileContents []byte
+	if err != nil {
+		fmt.Println("unable to get current directory")
+	}
+	if newFileContents, err = ChangeValue("main.tf", basePath, "apigee-x-core", "source", "mysource"); err != nil {
+		fmt.Println("error in ChangeValue ", err.Error())
+	}
+	os.WriteFile("main.tf", newFileContents, 0644)
+}
+
+func ChangeValue(fileName, filePath, label, key, value string) ([]byte, error) {
+	targetFile := filepath.Join(filePath, fileName)
+	file, err := ioutil.ReadFile(targetFile)
+	var indexOfBlock int8 = 0
+
+	if err != nil {
+		return nil, err
+	}
+
+	hclFile, hclErrors := hclwrite.ParseConfig(file, fileName, hcl.InitialPos)
+	if hclErrors.HasErrors() {
+		return nil, hclErrors
+	}
+
+	if indexOfBlock = getBlockIndex(hclFile, label); indexOfBlock == -1 {
+		return nil, fmt.Errorf("unable to find any block with given label")
+	}
+
+	blockToChange := hclFile.Body().Blocks()[indexOfBlock]
+	blockToChange.Body().SetAttributeValue(key, cty.StringVal(value))
+
+	return hclFile.Bytes(), nil
+}
+
+func getBlockIndex(file *hclwrite.File, label string) int8 {
+	var blockIndex int8 = -1
+	for index, block := range file.Body().Blocks() {
+		if contains(label, block.Labels()) {
+			blockIndex = int8(index)
+			break
+		}
+	}
+	return blockIndex
+}
+
+func contains(word string, words []string) bool {
+	i := sort.SearchStrings(words, word)
+	return i < len(words) && words[i] == word
+}
