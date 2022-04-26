e38dbb72e4941e229b13444afd28cb53a2365db6
Prateek Khushalani <prateek.khushalani@gmail.com>
2022-04-26 17:08:37 +0530

    Exercise 2: Get the Variables from variables tf file

--- hclwrite/main.go
+++ hclwrite/main.go
@@ -9,20 +9,22 @@ import (
 
 	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/hclwrite"
+	"github.com/hashicorp/terraform-config-inspect/tfconfig"
 	"github.com/zclconf/go-cty/cty"
 )
 
 func main() {
 	fmt.Println("hello WOrld")
 	basePath, err := os.Getwd()
-	var newFileContents []byte
+	// var newFileContents []byte
 	if err != nil {
 		fmt.Println("unable to get current directory")
 	}
-	if newFileContents, err = ChangeValue("main.tf", basePath, "apigee-x-core", "source", "mysource"); err != nil {
-		fmt.Println("error in ChangeValue ", err.Error())
-	}
-	os.WriteFile("main.tf", newFileContents, 0644)
+	// if newFileContents, err = ChangeValue("main.tf", basePath, "apigee-x-core", "source", "mysource"); err != nil {
+	// 	fmt.Println("error in ChangeValue ", err.Error())
+	// }
+	// os.WriteFile("main.tf", newFileContents, 0644)
+	GetTFKeys(basePath)
 }
 
 func ChangeValue(fileName, filePath, label, key, value string) ([]byte, error) {
@@ -63,4 +65,17 @@ func getBlockIndex(file *hclwrite.File, label string) int8 {
 func contains(word string, words []string) bool {
 	i := sort.SearchStrings(words, word)
 	return i < len(words) && words[i] == word
+}
+
+func GetTFKeys(filePath string) (map[string]string, error) {
+
+	module, diags := tfconfig.LoadModule(filePath)
+	if diags.HasErrors() {
+		return nil, diags
+	}
+	tfKeys := make(map[string]string, len(module.Variables))
+	for _, variable := range module.Variables {
+		tfKeys[variable.Name] = variable.Type
+	}
+	return tfKeys, nil
 }
